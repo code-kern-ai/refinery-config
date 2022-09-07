7bd3838c33f4702744ac1212a4de4fabed34f50e
Johannes Hötter <johannes.hoetter@kern.ai>
2022-09-07 16:01:30 +0200

    adds try catch

--- notify_handler.py
+++ notify_handler.py
@@ -7,6 +7,9 @@ def notify_others_about_change(notify: Dict[str, str]) -> None:
 
     for key in notify:
         url = f"{notify[key]}/config_changed"
-        response = requests.put(url)
-        if response.status_code != 200:
-            print("cound't notify ", key)
+        try:
+            response = requests.put(url)
+            if response.status_code != 200:
+                print("couldn't notify ", key)
+        except requests.exceptions.ConnectionError as e:
+            print("couldn't notify ", key)
