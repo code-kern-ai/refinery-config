eeb596891fc7416b499ef8e247cd01449183fc1a
Felix Kirsch <40427105+FelixKirsch@users.noreply.github.com>
2022-09-12 13:48:27 +0200

    Release v1.2.0 (#8)
    * Update LICENSE
    
    * Adds field for is_demo check
    
    * Adds is demo logic
    
    * Removes is_demo from blacklist to access form config manager
    
    * Readds demo to blacklist
    
    * Create dependabot.yml
    
    * Adds embedder and zero-shot to services_to_notify (#6)
    
    * adds embedder and zero-shot to services_to_notify
    
    * Adds notifcation of current config values on startup (without gateway)
    
    Co-authored-by: JWittmeyer <jens.wittmeyer@onetask.ai>
    
    * adds try catch (#7)
    
    * adds try catch
    
    * adds general catch
    
    Co-authored-by: JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
    Co-authored-by: simonrempel <simon.degraf@icloud.com>
    Co-authored-by: JWittmeyer <jens.wittmeyer@onetask.ai>
    Co-authored-by: Johannes Hötter <johannes.hoetter@kern.ai>

--- notify_handler.py
+++ notify_handler.py
@@ -1,12 +1,18 @@
 from typing import Dict
 
 import requests
+import traceback
 
 
 def notify_others_about_change(notify: Dict[str, str]) -> None:
 
     for key in notify:
         url = f"{notify[key]}/config_changed"
-        response = requests.put(url)
-        if response.status_code != 200:
-            print("cound't notify ", key)
+        try:
+            response = requests.put(url)
+            if response.status_code != 200:
+                print("couldn't notify ", key)
+        except requests.exceptions.ConnectionError:
+            print("couldn't notify ", key)
+        except Exception:
+            print(traceback.format_exc(), flush=True)
